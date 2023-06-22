d6f757363389c104ab34b670af1bc16dc79ed13c
Narinder Maurya <narinder06@pm.me>
2023-06-22 14:50:23 +0530

    Complexity Done

---
+++ Time Complexity/basics.cpp
@@ -0,0 +1,67 @@
+it is not the time taken it is dependent on system
+
+
+
+"rate at which the time taken increases wr.r.t size"
+
+
+
+old windows machine             new macbook
+
+    theta{rate of inc}          theta{rate of inc}
+
+T.C
+
+in terms of BIG- On Notation => O()
+
+for (int i = 1; i < 5; i++)
+{
+    cout << "raj";
+}
+total number of steps======> Insane if done manually
+
+
+
+'in terms of worst case scenario
+'avoid constants
+'avoid lower values
+
+O(15)=== NOT APPROPRIATE
+
+if i<n
+=>   O(N x 3)
+
+best average worst case
+
+<25 =D
+<45 =C
+<65 = B
+else = A
+
+
+
+marsk=10 > 2 steps
+m=70 > 4 STEPS
+
+
+upper bound complexity -- BIG ON
+lowe bond- OMEGA
+Average -- Theta 
+
+Space Complexity-----
+memory space
+big-O notation
+Auxillary space + input space
+take to solve  + to store the problemn
+
+c=a+b
+aux
+
+
+inta[n];
+b=a+b;
+avoid lesser space but not in interview
+take extra variables
+
+
+1s= 10^8 operations
